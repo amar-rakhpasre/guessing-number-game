f13aee9258b3d48e120d6bd2d7c7114aa900999c
Amar Rakhpasre <154906069+amar-rakhpasre@users.noreply.github.com>
2024-06-19 22:14:07 +0530

    Update web.js

--- five_pro/web.js
+++ five_pro/web.js
@@ -1,48 +1,43 @@
 const max = 1000;
 const min = 1;
-const answer = Math.floor(Math.random() * (max-min + 1)) + min;
-
+const answer = Math.floor(Math.random() * (max - min + 1)) + min;
 
 let attempts = 0;
 let guess;
 let running = true;
 
-while(running){
-
-    guess = window.prompt(`guess a number between ${min}-${max}`)
+while (running) {
+    guess = window.prompt(`Guess a number between ${min}-${max}`);
     guess = Number(guess);
 
-    if(isNaN(guess)){
-        window.alert("Please Enter a Valid Number ");
-
-    }else if(guess < min || guess > max){
-        window.alert("Please Enter a Valid Number");
-    }else{
-        attempts++
-        if(guess < answer){
+    if (isNaN(guess)) {
+        window.alert("Please enter a valid number");
+    } else if (guess < min || guess > max) {
+        window.alert("Please enter a number between the specified range");
+    } else {
+        attempts++;
+        if (guess < answer) {
             window.alert("TOO LOW! TRY AGAIN!");
-        }else if(guess > answer){
+        } else if (guess > answer) {
             window.alert("TOO HIGH! TRY AGAIN!");
-        }else{
-            window.alert(`CORRECT! The Answer was ${answer}. It Took you ${attempts} Attempts`);
+        } else {
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attempts`);
             running = false;
-            
         }
     }
-
-}  
+}
 
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
-let width = canvas.width = window.innerWidth;
-let height = canvas.height = window.innerHeight;
+let width = (canvas.width = window.innerWidth);
+let height = (canvas.height = window.innerHeight);
 let str = "A+jk js:2 @dfs 17 tr YY ufds M5r P87 #18 $!& ^dfs $Ew er JH # $ * . (! ;) ,: :";
 let matrix = str.split("");
 let font = 12;
 let col = width / font;
 let arr = [];
 
-for(let i = 0; i < col; i++) {
+for (let i = 0; i < col; i++) {
     arr[i] = 1;
 }
 
@@ -52,18 +47,17 @@ const draw = () => {
     ctx.fillStyle = "#00ff00";
     ctx.font = `${font}px system-ui`;
 
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let txt = matrix[Math.floor(Math.random() * matrix.length)];
         ctx.fillText(txt, i * font, arr[i] * font);
 
-        if(arr[i] * font > height && Math.random() > 0.975) {
+        if (arr[i] * font > height && Math.random() > 0.975) {
             arr[i] = 0;
         }
         arr[i]++;
     }
-}
+};
 
 setInterval(draw, 20);
 
 window.addEventListener("resize", () => location.reload());
-
